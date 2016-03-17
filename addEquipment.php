a1486adc1a33ba94009a9270938b0f786474eeb7
Onyinye <soguego@yahoo.com>
2016-03-17 22:18:33 +0000

    complete addEquipment file

--- addEquipment.php
+++ addEquipment.php
@@ -21,7 +21,7 @@
 	if(!isset($_REQUEST['name'])){
 		exit();		
 		//if no data, exit
-	}
+	
 	//print_r($_REQUEST);
 	$name=$_REQUEST['name'];
 	$barcode=$_REQUEST['barcode'];
@@ -30,73 +30,12 @@
 	$labLocation=$_REQUEST['labLocation'];
 	$dateAdded=$_REQUEST['dateAdded'];
 	include_once("equipment.php");
-
-	
-	
-	$strQuery="insert into equipments set NAME='$name', BARCODE='$barcode',
-				DESCRIPTION='$desc',
-				DATE='$dateAdded',
-				LABNAME='$labName',
-				LABLOCATION='$labLocation'";
-				
-	//echo $strQuery;
-	if($db->query($strQuery)){
-		echo "Data Added";
-	}else{
-		echo "Error while adding data";
-	}
-	
-	if($db->query($strQuery)){
-		echo "Data Added";
-	}else{
-		echo "Error while adding data";
-	}
-	$result=$db->query("select * from equipments");
-	
-	//Print table
-	$col1="pink";
-			$col2="purple";
-			$col3="#cccccc";
-			$col4="#e9e9e9";
-			$counter=1;
-	
-	echo "<table border='1'; bgcolor=$col4>
-	
-			<tr bgcolor=$col3>
-				<td>Equipment Name</td>
-				<td>Barcode</td>
-				<td>Description</td>
-				<td>Lab Name</td>
-				<td>Lab Location</td>
-			</tr>";
-	
-	$row=$result->fetch_assoc();
-	while($row){
-		if ($counter==1){
-	
-		echo "<tr>
-			<td bgcolor=$col3>{$row['NAME']}</td>
-			<td bgcolor=$col1>{$row['BARCODE']}</td>
-			<td bgcolor=$col1>{$row['DESCRIPTION']}</td>
-			<td bgcolor=$col1>{$row['LABNAME']}</td>
-			<td bgcolor=$col1>{$row['LABLOCATION']}</td>
-			
-			</tr>";
-		$row=$result->fetch_assoc();
-		$counter=0;
+	$obj=new equipment();
+	$r=$obj->addEquipment($name,$barcode,$desc,$dateAdded,$labName,$labLocation);
+	if($r==false){
+		$strStatusMessage="error while adding Equipment";
 		}else{
-			echo "<tr>
-			<td bgcolor=$col3>{$row['NAME']}</td>
-			<td bgcolor=$col1>{$row['BARCODE']}</td>
-			<td bgcolor=$col1>{$row['DESCRIPTION']}</td>
-			<td bgcolor=$col1>{$row['LABNAME']}</td>
-			<td bgcolor=$col1>{$row['LABLOCATION']}</td>
-			</tr>";
-		$row=$result->fetch_assoc();
-		$counter=1;
-			
-			
+					$strStatusMessage="$name added";
 		}
 	}
-	echo "</table>";
 ?>
