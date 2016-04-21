62fbf4a45bc4dc75e215183e9d0b46f370793c97
Onyinye <soguego@yahoo.com>
2016-04-21 23:26:29 +0100

    Commenting and formatting

--- equipAjax.php
+++ equipAjax.php
@@ -1,44 +1,45 @@
 <?php
 
-if (isset($_REQUEST['cmd'])){
-	$cmd=$_REQUEST['cmd'];
-	switch($cmd){
-		case 1:
-		addEquip();
-		break;
-		default;
-		echo "wrong command";
-		break;
+	if (isset($_REQUEST['cmd'])){
+		$cmd=$_REQUEST['cmd'];
+			switch($cmd){
+				case 1:
+				addEquip();
+				break;
+				default;
+				echo "wrong command";
+				break;
+			}
 	}
-}
-function addEquip(){
-	if(!isset($_REQUEST['name'])){
-		exit();		
+
+	function addEquip(){
 		//if no data, exit
-	}
-	
-	//print_r($_REQUEST);
-	$name=$_REQUEST['name'];
-	$barcode=$_REQUEST['barcode'];
-	$desc=$_REQUEST['desc'];
-	$labName=$_REQUEST['labName'];
-	$labLocation=$_REQUEST['labLocation'];
-	$dateAdded=$_REQUEST['dateAdded'];
-	
-	include_once("equipment.php");
-	$obj=new equipment();
-	$r=$obj->addEquipment($name,$barcode,$desc,$dateAdded,$labName,$labLocation);
-	
-	//Print message
-	if($r==false){
-		$strStatusMessage="error while adding Equipment";
-		
-		}else{
-		$strStatusMessage="$name added";
+		if(!isset($_REQUEST['name'])){
+			exit();		
 		}
-		echo $strStatusMessage;
-	
-	
+		
+		//declare variables
+		//print_r($_REQUEST);
+		$name=$_REQUEST['name'];
+		$barcode=$_REQUEST['barcode'];
+		$desc=$_REQUEST['desc'];
+		$labName=$_REQUEST['labName'];
+		$labLocation=$_REQUEST['labLocation'];
+		$dateAdded=$_REQUEST['dateAdded'];
+		
+		include_once("equipment.php");
+		$obj=new equipment();
+		$r=$obj->addEquipment($name,$barcode,$desc,$dateAdded,$labName,$labLocation);
+		
+		//Print message
+		if($r==false){
+			$strStatusMessage="error while adding Equipment";
+		}else{
+			$strStatusMessage="$name added";
+		}
+			echo $strStatusMessage;
+		
+		
 	}
-	
+		
 ?>
